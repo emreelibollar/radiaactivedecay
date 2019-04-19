4c34aafe829adfa1b5fca6fad55feb89980719df
emreelibollar <45038075+emreelibollar@users.noreply.github.com>
2019-04-19 21:12:39 +0300

    Add files via upload

--- a35kv.m
+++ a35kv.m
@@ -12,4 +12,4 @@ SS3=0.35./s3_5kv15;
 
 z1= linfitxy(t3_5kv,S1,xerror,SS1);
 z2= linfitxy(t3_5kv10,S2,xerror,SS2);
-z3= linfitxy(t3_5kv15,S3,xerror,SS3);
+z3= linfitxy(t3_5kv15,S3,xerror1,SS3);
--- a45kv.m
+++ a45kv.m
@@ -1,3 +1,4 @@
+clc
 
 S1=log(s4_5kv);
 S2=log(s4_5kv10);
@@ -10,5 +11,5 @@ SS3=0.35./s4_5kv15;
 
 
 z1= linfitxy(t4_5kv,S1,xerror,SS1);
-z2= linfitxy(t4_5kv10,S2,xerror,SS2);
-z3= linfitxy(t4_5kv15,S3,xerror,SS3);
+z2= linfitxy(t4_5kv10,S2,xerror1,SS2);
+z3= linfitxy(t4_5kv15,S3,xerror1,SS3);
